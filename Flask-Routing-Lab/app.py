e75cc0e3a4608611b42bf7962cb91f9070fe183b
Nadeengh24-meet <139760345+Nadeengh24-meet@users.noreply.github.com>
2023-07-19 21:42:25 +0300

    still working

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -11,9 +11,11 @@ app = Flask(  # Create a flask app
 @app.route('/')
 def home():
     return render_template("home.html")
+
 @app.route("/product")
 def product():
     return render_template("product.html")
+
 @app.route("/cart")
 def cart():
     return render_template("cart.html")
